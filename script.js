ace3f4e423d8d9b8989306b503623718fe5d0d3a
Pablo Kauan <pablokauanoficial@gmail.com>
2023-06-16 20:58:53 -0300

    registro-add

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ document.querySelector(".formulario-js").addEventListener("submit", (e) => {
   const email = document.querySelector("#email").value.toLowerCase();
   const senha = document.querySelector("#senha").value;
   const dados =
-    JSON.parse(localStorage.getItem("dados")) ||
+    JSON.parse(sessionStorage.getItem("dados")) ||
     popUp("Nenhum email cadastrado", 2000);
   if (dados) {
     const emailCadastrado =
@@ -20,12 +20,38 @@ document.querySelector(".formulario-js").addEventListener("submit", (e) => {
   }
 });
 //Form Cadastro
-document
-  .querySelector(".formularioCadastro-js")
-  .addEventListener("submit", (e) => {
-    e.preventDefault();
-    popUp("Cadastro indisponível", 1700);
+function initCadastro() {
+  const formCadast = document.querySelector(".formularioCadastro-js");
+  const dados = JSON.parse(sessionStorage.getItem("dados")) || [];
+
+  formCadast.addEventListener("submit", (e) => {
+    const inputEmail = document.querySelector("#emailCadastro").value;
+    const inputNome = document.querySelector("#nomeCadastro").value;
+    const inputSenha = document.querySelector("#senhaCadastro").value;
+    const inputSenhaConfir = document.querySelector(
+      "#senhaConfirmacaoCadastro"
+    ).value;
+    if (inputSenha === inputSenhaConfir) {
+      const usuario = {
+        nome: inputNome,
+        email: inputEmail,
+        senha: inputSenha,
+      };
+      const verificarDados = dados.find((item) => usuario.email === item.email);
+      console.log(verificarDados);
+      if (!verificarDados) {
+        dados.push(usuario);
+        sessionStorage.setItem("dados", JSON.stringify(dados));
+      } else {
+        e.preventDefault();
+        popUp("Email já cadastrado", 1500);
+      }
+    } else {
+      popUp("Senhas não coincidem", 1700);
+    }
   });
+}
+initCadastro();
 
 //Animação formulario
 function trocarForm(primeiroForm, proximoForm) {
@@ -47,20 +73,16 @@ function popUp(mensagem, tempo) {
     const gridCentral = document.querySelector(".gridCentral-js");
     gridCentral.insertAdjacentHTML("beforebegin", elementoAviso);
     setTimeout(() => {
-      const elementoNoDom = document.querySelector(".alerta-Js");
-      elementoNoDom.classList.replace("slide-in-top", "slide-out-top");
-      elementoNoDom.addEventListener("animationend", () =>
-        elementoNoDom.remove()
-      );
+      const elementoDom = document.querySelector(".alerta-Js");
+      elementoDom.classList.replace("slide-in-top", "slide-out-top");
+      elementoDom.addEventListener("animationend", () => elementoDom.remove());
     }, tempo);
   }
 }
 //Mostar Senha
-
 document.querySelectorAll(".detalhe-js").forEach((item) => {
   item.addEventListener("click", () => mostarSenha(item));
 });
-
 function mostarSenha(item) {
   const input =
     item.parentElement.querySelector("#senha") ||
