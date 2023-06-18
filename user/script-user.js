4b65f744dda35b1fd72721e63835f37b73a12285
Pablo Kauan <pablokauanoficial@gmail.com>
2023-06-18 16:13:51 -0300

    config-add

--- user/script-user.js
+++ user/script-user.js
@@ -7,10 +7,32 @@ if (sessionStorage.getItem("user")) {
   document.head.querySelector("title").innerText =
     nome[0].toUpperCase() + nome.substring(1).split(" ")[0];
 
-  document.querySelector(".configuracao").addEventListener("click", () => {
-    localStorage.clear();
-    window.history.back()
-  });
+  function abrirConfig() {
+    const configuracao = document.querySelector(".configuracao");
+    configuracao.addEventListener("click", () => {
+      const configDom = document.querySelector(".config-user-js");
+      if (!configDom) {
+        configuracao.classList.add("rotate-in-center");
+        const configDomElemnt = `<div class='config-user-js slide-in-bottom'><img src='icons/rafiki.svg'><ul class='lista-dados-js'><li class='lista-dados-items-js'><p class='texto-info-js'>Apelido</p><span class='nome-js'>${nome}</span></li><li class='lista-dados-items-js'><p class='texto-info-js'>Email</p><span class='email-js'>${email}</span></li><li class='lista-dados-items-js'><p class='texto-info-js'>Senha</p><span class='senha-js'>Trocar Senha</span></li></ul><button class='botaoSair-js'>Sair</button></div>`;
+        document
+          .querySelector(".principal")
+          .insertAdjacentHTML("afterend", configDomElemnt);
+        //apagar depois
+        document
+          .querySelector(".botaoSair-js")
+          .addEventListener("click", () => {
+            sessionStorage.clear();
+            localStorage.clear();
+            window.history.back();
+          });
+      } else {
+        configuracao.classList.remove("rotate-in-center");
+        configDom.classList.replace("slide-in-bottom", "slide-out-bottom");
+        configDom.addEventListener("animationend", () => configDom.remove());
+      }
+    });
+  }
+  abrirConfig();
 } else {
   alert("Você não está logado");
   document.querySelector(".principal").style.display = "none";
