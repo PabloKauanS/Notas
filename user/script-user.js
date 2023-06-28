99c1aeb20a7467ccf497aa0945504e9f0d58188c
Pablo Kauan <pablokauanoficial@gmail.com>
2023-06-27 21:29:36 -0300

    page-de-adicionar-notas

--- user/script-user.js
+++ user/script-user.js
@@ -6,11 +6,13 @@ if (dataUser) {
     const userName = dataUser.name;
     htmlHead.innerText = assembleName(userName);
     function assembleName(userName) {
-      return userName[0].toUpperCase() + userName.substring(1).split(" ")[0];
+      return (
+        userName[0].toUpperCase() +
+        userName.substring(1).split(" ")[0].toLowerCase()
+      );
     }
   }
   initNameTitle();
-
   //Open Setting
   function initSettingOpen() {
     const settingDom = document.querySelector(".setting");
@@ -38,12 +40,40 @@ if (dataUser) {
       );
     }
     function createElement(dataUser) {
-      return `<div class='setting-user-js slide-in-bottom'><img src='icons/rafiki.svg'><ul class='listData-js'><li class='listData-item-js'><p class='text-info-js'>Apelido</p><span class='name-js'>${dataUser.name}</span></li><li class='listData-item-js'><p class='text-info-js'>Email</p><span class='email-js'>${dataUser.email}</span></li><li class='listData-item-js'><p class='text-info-js'>Senha</p><span class='pass-js'>Trocar Senha</span></li></ul><button class='button-js'>Sair</button></div>`;
+      return `<div class='setting-user-js slide-in-bottom'><img src='icons/rafiki.svg'><ul class='listData-js'><li class='listData-item-js'><p class='text-info-js'>Apelido</p><span class='name-js'>${dataUser.name}</span></li><li class='listData-item-js'><p class='text-info-js'>Email</p><span class='email-js'>${dataUser.email}</span></li><li class='listData-item-js'><p class='text-info-js'>Senha</p><span class='pass-js'>Trocar Senha</span></li></ul><button class='button-js' onclick="initExit()">Sair</button></div>`;
     }
   }
   initSettingOpen();
- 
+  //Exit
+  function initExit() {
+    sessionStorage.clear();
+    window.history.back();
+  }
+  //Add notes
+  function initNotation() {
+    const buttonCircle = document.querySelector(".detailCircle");
+    buttonCircle.addEventListener("click", openAddNote);
+    function openAddNote() {
+      const mainDom = document.querySelector(".main");
+      const elementNotation = noteElement();
+      mainDom.insertAdjacentHTML("afterend", elementNotation);
+    }
+    function noteElement() {
+      return `<div class="notation-js slide-in-bottom">
+      <span class="notationDetail-js"></span>
+      <form action="" method="get" class="notationForm-js">
+          <input type="text" placeholder="Título" id="title" name='title'>
+          <textarea name="description" id="description" placeholder="Descrição" cols="30" rows="10"></textarea>
+          <input type="date" name="date" id="date" disabled>
+          <input type="time" id="time" disabled>
+          <button class="notationButton-js">Adicionar</button>
+      </form>
+  </div>`;
+    }
+  }
+  initNotation();
+  //
 } else {
-  document.querySelector(".main").style.display = "none";
+  document.body.style.display = "none";
   alert("Você não está logado");
 }
